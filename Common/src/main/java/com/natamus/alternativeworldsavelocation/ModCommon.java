2b72dfb72a21e7a1133239ad1cded0a24e2d8b5f
Rick South <rick@natamus.com>
2025-07-18 03:14:48 +0200

    Alternative World Save Location source code for 1.21.8.

---
+++ Common/src/main/java/com/natamus/alternativeworldsavelocation/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.alternativeworldsavelocation;
+
+import com.natamus.alternativeworldsavelocation.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
